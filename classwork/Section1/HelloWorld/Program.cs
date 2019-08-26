05948c0adc19f382dd1666f9cc99cf8792e80dfe
mohammed.rayed <mohammed.rayed@NEITD-211201>
2019-08-26 18:51:30 -0500

    Update

--- classwork/Section1/HelloWorld/Program.cs
+++ classwork/Section1/HelloWorld/Program.cs
@@ -13,13 +13,21 @@ namespace HelloWorld
             //Definitly assigned
             // name = "Bob";
             string name2 = Console.ReadLine ();
-            name2 = Console.ReadLine ();
+            //name2 = Console.ReadLine ();
 
             name2 = name = "Sue";
-
+            Console.WriteLine (name);
             Console.WriteLine (name2);
+            Console.WriteLine ("Hello World! ");
 
-           // Console.WriteLine ("Hello World! ");
+            int hours = 8;
+            double payRate = 15.25;
+
+            double totalPay = payRate * hours;
+
+            string fullName = "Fred" + " " + "Jones";
+            Console.WriteLine (fullName);
+            Console.WriteLine (totalPay);
         }
     }
 }
