c4e797294bee7f245156da374e2d2ea418c8921c
Mohammed Rayed <mohammed.rayed@my.tccd.edu>
2019-09-15 14:49:56 -0500

    Story 2 Finished

---
+++ labs/Lab1/Itse1430.Maze/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Itse1430.Maze
+{
+    class Program
+    {
+        static void Main( /*string[] args*/ )
+        {
+
+            RoomOne ();
+            RoomTwo ();
+            RoomThree ();
+            RoomFour ();
+            RoomFive ();
+            RoomSix ();
+            RoomSeven ();
+            RoomEight ();
+            RoomNine ();
+            RoomTen ();
+            RoomEleven ();
+            RoomTwelve ();
+            RoomThirteen ();
+            RoomFourteen ();
+            RoomFifteen ();
+            RoomSixteen ();
+            RoomSeventeen ();
+        }
+        static void RoomOne ()
+        {
+            Console.WriteLine ("Oh no! the entrance beside you was blocked off. You have to find a way out! Find the room with the Exit.");
+            Console.WriteLine ("You've entered Room 1");
+        }
+        static void RoomTwo ()
+        {
+            Console.WriteLine ("You've entered Room 2");
+        }
+        static void RoomThree ()
+        {
+            Console.WriteLine ("You've entered Room 3");
+        }
+        static void RoomFour ()
+        {
+            Console.WriteLine ("You've entered Room 4");
+        }
+        static void RoomFive ()
+        {
+            Console.WriteLine ("You've entered Room 5");
+        }
+        static void RoomSix ()
+        {
+            Console.WriteLine ("You've entered Room 6");
+        }
+        static void RoomSeven ()
+        {
+            Console.WriteLine ("You've entered Room 7");
+        }
+        static void RoomEight ()
+        {
+            Console.WriteLine ("You've entered Room 8");
+        }
+        static void RoomNine ()
+        {
+            Console.WriteLine ("You've entered Room 9");
+        }
+        static void RoomTen ()
+        {
+            Console.WriteLine ("You've entered Room 10");
+        }
+        static void RoomEleven ()
+        {
+            Console.WriteLine ("You've entered Room 11");
+        }
+        static void RoomTwelve ()
+        {
+            Console.WriteLine ("You've entered Room 12");
+        }
+        static void RoomThirteen ()
+        {
+            Console.WriteLine ("You've entered Room 13");
+        }
+        static void RoomFourteen ()
+        {
+            Console.WriteLine ("You've entered Room 14");
+        }
+        static void RoomFifteen ()
+        {
+            Console.WriteLine ("You've entered Room 15");
+        }
+        static void RoomSixteen ()
+        {
+            Console.WriteLine ("You've entered Room 16");
+        }
+        static void RoomSeventeen ()
+        {
+            Console.WriteLine ("You've entered Room 17");
+        }
+    }
+}
+
